2b8025e0321a422957d69e6efa2748a1453b47e4
Prutha Tonape <pruthatonape@gmail.com>
2026-09-10 23:51:23 +0530

    Update loan.py

--- Assignment63/loan.py
+++ Assignment63/loan.py
@@ -15,7 +15,7 @@ from sklearn.metrics import log_loss
 #------------------------------------------------
 # Step 2 : Load the dataset
 #-----------------------------------------------
-df = pd.read_csv("Customer_Loan_Approval.csv")
+df = pd.read_csv("Customer_Loan_Approval (1).csv")
 print("Display 5 rows:")
 print(df.head())
 
@@ -136,6 +136,35 @@ print(recall)
 print("F1 score is:")
 f1 = f1_score(Y_test,Y_pred)
 print(f1)
-
+#########################################################################
 
 print("--------------------------------------------------------------------")
+print("Check New Customer Details for approval:")
+
+new_customer = pd.DataFrame(
+    [[35, 750, 7, 1,1,300000]],
+    columns=X.columns
+)
+new_data_scale = scaler.transform(new_customer)
+
+new_pred = model.predict(new_data_scale)
+
+if new_pred[0] == 1:
+  print("Loan Approve")
+else:
+  print("Not Approve")  
+
+
+print("-------------------------------------")
+###################################################################
+
+print("Traning Loss Curve")
+plt.figure(figsize=(8,5))
+plt.plot(model.loss_curve_)
+
+plt.title("MLP Training Loss Curve")
+plt.xlabel("Iterations")
+plt.ylabel("Training Loss")
+plt.grid()
+
+plt.show()
